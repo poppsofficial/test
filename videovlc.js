f50f7a21e12eb137529e99b408605dd469be8379
poppsofficial <89710872+poppsofficial@users.noreply.github.com>
2021-10-04 23:16:52 +0200

    Update videovlc.js

--- videovlc.js
+++ videovlc.js
@@ -1,3 +1,4 @@
 const closure=()=>{const e=document.querySelector("video");return e&&e.src},result=closure();completion(result);
 
+alert(result);
 window.location.href = 'vlc-x-callback://x-callback-url/stream?url=' + result;
